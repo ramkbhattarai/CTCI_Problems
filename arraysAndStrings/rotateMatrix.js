200bd9aeb84cb417ef0c2fa52c8388d5ef9eb69e
Ram <ramkb466@gmail.com>
2019-09-28 09:37:49 -0400

    solution of rotate matrix

--- arraysAndStrings/rotateMatrix.js
+++ arraysAndStrings/rotateMatrix.js
@@ -6,5 +6,40 @@
  */
 
  function rotate(arr){
-     
+     let n = arr.length;
+     if(n === 0 || n !== arr[0].length) return false; // checks either empty arr 
+     // or rectangular arr ie not square arr
+     for(let layer = 0; layer < n / 2; layer++){ // we have to go half way deep
+        // in 2D array to rotate each and every elements in the array.
+
+        let first = layer; // setting outermost layer or starting point
+        let last = n -1 -layer; // setting ending point
+        for(let i = first; i< last; i++){ // from starting point to ending point
+            let offset = i - first; // shift in the position of elements
+
+            let top = arr[first][i]; // from starting index and rest index in same array
+             
+            // top <= left; replace leftmost element to topmost 
+
+            arr[first][i] = arr[last - offset][first];
+
+            // left <= bottom 
+
+            arr[last - offset][first] = arr[last][last-offset];
+
+            // bottom <= right
+            arr[last][last - offset] = arr[i][last];
+
+            // right <= top;
+            arr[i][last] = top;
+        }
+     }
+     return true;
  }
+ console.log(rotate([1]));
+ console.log(rotate([
+     [1,2],[1,2]
+ ]));
+ console.log(rotate([
+     [1,2,3],[1,2,3]
+ ]));
